7c727e1bd194ca0c49574931ee3c9f679a12a8a3
dag-bag <virenderkumar23435@gmail.com>
2022-06-19 13:07:16 +0530

    main

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,24 @@ import Image from "next/image";
 import Features from "../components/Features";
 import Products from "../components/Products";
 import Product from "../models/Product";
+import Script from "next/script";
 import mongoose, { mongo } from "mongoose";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 export default function Home({ addToCart, Cart, removeFromCart, products }) {
   return (
     <div>
+      <Script
+        id="my-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-55GFBMB');`,
+        }}
+      ></Script>
       <Head>
         <title>Best packaging site - aulia.</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
