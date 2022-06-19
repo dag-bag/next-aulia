daf6c8aee025574ee01768ef2ab1daf759e4008c
dag-bag <virenderkumar23435@gmail.com>
2022-06-19 13:10:04 +0530

    main

--- pages/index.js
+++ pages/index.js
@@ -11,17 +11,6 @@ import "react-toastify/dist/ReactToastify.css";
 export default function Home({ addToCart, Cart, removeFromCart, products }) {
   return (
     <div>
-      <Script
-        id="my-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-55GFBMB');`,
-        }}
-      ></Script>
       <Head>
         <title>Best packaging site - aulia.</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
