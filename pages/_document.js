bee7d233d53f95ce9b74ffa092173c1b5c6127bc
dag-bag <virenderkumar23435@gmail.com>
2022-06-19 12:34:01 +0530

    main

--- pages/_document.js
+++ pages/_document.js
@@ -4,19 +4,18 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <Script
-          id="1"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <Script
+        id="1"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-55GFBMB');`,
-          }}
-        ></Script>
-      </Head>
+        }}
+      ></Script>
+      <Head />
       <body>
         <noscript
           dangerouslySetInnerHTML={{
