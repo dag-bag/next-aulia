526c103aa667b14b8a20c6991657a8815f5df2fb
dag-bag <virenderkumar23435@gmail.com>
2022-06-19 14:00:08 +0530

    main

--- pages/_app.js
+++ pages/_app.js
@@ -185,7 +185,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Script
         id="my-script"
-        // strategy="lazyOnload"
+        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
