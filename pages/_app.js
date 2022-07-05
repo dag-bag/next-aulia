ac91e4124fbfe82c7a39b87b3a32c382b76c3e1f
dag-bag <virenderkumar23435@gmail.com>
2022-07-05 13:54:25 +0530

    main

--- pages/_app.js
+++ pages/_app.js
@@ -68,32 +68,35 @@ function MyApp({ Component, pageProps }) {
     window.dataLayer.push({
       event: "add_to_cart",
       ecommerce: {
-        items: [
-          {
-            item_id: "SKU_12345",
-            item_name: "Stan and Friends Tee",
-            affiliation: "Google Merchandise Store",
-            coupon: "SUMMER_FUN",
-            currency: "USD",
-            discount: 2.22,
-            index: 0,
-            item_brand: "Google",
-            item_category: "Apparel",
-            item_category2: "Adult",
-            item_category3: "Shirts",
-            item_category4: "Crew",
-            item_category5: "Short sleeve",
-            item_list_id: "related_products",
-            item_list_name: "Related Products",
-            item_variant: "green",
-            location_id: "L_12345",
-            price: 9.99,
-            quantity: 1,
-          },
-        ],
+        data: {
+          item_id: "SKU_12345",
+          item_name: "Stan and Friends Tee",
+          affiliation: "Google Merchandise Store",
+          coupon: "SUMMER_FUN",
+          currency: "USD",
+          price: 5.44,
+          discount: 2.22,
+          items: [
+            {
+              index: 0,
+              item_brand: "Google",
+              item_category: "Apparel",
+              item_category2: "Adult",
+              item_category3: "Shirts",
+              item_category4: "Crew",
+              item_category5: "Short sleeve",
+              item_list_id: "related_products",
+              item_list_name: "Related Products",
+              item_variant: "green",
+              location_id: "L_12345",
+              price: 9.99,
+              quantity: 1,
+            },
+          ],
+        },
       },
     });
-    console.log(window.datalayer);
+
     toast.success("Item Added to cart", {
       position: "bottom-center",
       autoClose: 3000,
